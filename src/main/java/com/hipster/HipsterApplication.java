2cf09e7c80dcf2bb8fdd780604706c1bbffdddab
joohyeongzz81 <joohyeongzz81@gmail.com>
2026-02-10 13:59:57 +0900

    init

---
+++ src/main/java/com/hipster/HipsterApplication.java
@@ -0,0 +1,13 @@
+package com.hipster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HipsterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HipsterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hipster/HipsterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hipster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HipsterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
